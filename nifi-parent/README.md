6401d18c8cc41cd3050f66cc05fb2e1e0b2a52f6
Midhlaj <midhlaj.vs@nexquare.io>
2020-01-17 11:42:12 +0530

    Kylo 0.9.3 update and NiFi changes

---
+++ nifi-parent/README.md
@@ -0,0 +1,10 @@
+# NiFi
+
+[![Docker Hub](https://img.shields.io/docker/automated/govcloud/docker-nifi.svg)][registry]
+
+[NiFi][nifi] docker image extended from the [official source][official].
+
+[official]:            https://nifi.apache.org
+[nifi]:                https://github.com/apache/nifi
+[github]:              https://github.com/govcloud/docker-nifi
+[registry]:            https://hub.docker.com/r/govcloud/docker-nifi
---
+++ nifi-parent/scripts/docker-entrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+set -e
+
+create_keystore() {
+    echo ${HOST}
+    ${NIFI_TOOLKIT_HOME}/bin/tls-toolkit.sh standalone -O -n "${HOST}" -S "${KEYSTORE_PASS}" -C "CN=${HOST}, OU=${OU}" -P "${TRUSTSTORE_PASS}" -o "${NIFI_TOOLKIT_HOME}"
+    mv ${NIFI_TOOLKIT_HOME}/${HOST}/keystore.jks ${NIFI_HOME}/conf/keystore.jks
+    chown nifi:nifi ${NIFI_HOME}/conf/keystore.jks
+    mv ${NIFI_TOOLKIT_HOME}/${HOST}/truststore.jks  ${NIFI_HOME}/conf/truststore.jks
+    chown nifi:nifi ${NIFI_HOME}/conf/truststore.jks
+}
+
+nifi_properties()  {
+    #cat "${NIFI_HOME}/conf/nifi.temp" > "${NIFI_HOME}/conf/nifi.properties"
+    echo "nifi.web.http.host=${HOST}.${HEADLESS}" >> "${NIFI_HOME}/conf/nifi.properties"
+    echo "nifi.web.https.host=${HOST}.${HEADLESS}" >> "${NIFI_HOME}/conf/nifi.properties"
+    echo "nifi.remote.input.host=${HOST}.${HEADLESS}" >> "${NIFI_HOME}/conf/nifi.properties"
+    echo "nifi.cluster.node.address=${HOST}.${HEADLESS}" >> "${NIFI_HOME}/conf/nifi.properties"
+    echo "nifi.zookeeper.connect.string=${NIFI_ZOOKEEPER_CONNECT_STRING}" >> "${NIFI_HOME}/conf/nifi.properties"
+}
+
+# Runtime adjustments
+create_keystore
+nifi_properties
+
+# Ensure correct permissions
+chown nifi:nifi /opt/nifi/data
+chown nifi:nifi /opt/nifi/flowfile_repository
+chown nifi:nifi /opt/nifi/content_repository
+chown nifi:nifi /opt/nifi/logs
+
+sh "$@"
